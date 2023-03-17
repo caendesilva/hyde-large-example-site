4690b33f67a7606a409ebec35fef3981fb12cb40
Caen De Silva <caen@desilva.se>
2023-03-17 21:29:03 +0100

    Create redirect in service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Hyde\Hyde;
+use Hyde\Foundation\HydeKernel;
+use Hyde\Support\Models\Redirect;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+       Hyde::kernel()->booting(function (HydeKernel $kernel) {
+           $kernel->pages()->addPage(new Redirect('posts.html', 'posts/', false));
+       });
     }
 }
