742690dbd9a23cb0bde95ea21081b478fbbab0a7
Caen De Silva <caen@desilva.se>
2023-03-18 14:15:00 +0100

    Create ExportKernelJsonBuildTask.php

---
+++ app/Actions/ExportKernelJsonBuildTask.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Actions;
+
+use Hyde\Framework\Features\BuildTasks\PostBuildTask;
+
+class ExportKernelJsonBuildTask extends PostBuildTask
+{
+    public function handle(): void
+    {
+        // TODO: Implement handle() method.
+    }
+}
