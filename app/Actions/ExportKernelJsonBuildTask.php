d02a61c25e0e34ac3700ac76b25ac421565c5bb2
Caen De Silva <caen@desilva.se>
2023-03-18 14:16:05 +0100

    Export Kernel as Json at build

--- app/Actions/ExportKernelJsonBuildTask.php
+++ app/Actions/ExportKernelJsonBuildTask.php
@@ -2,12 +2,16 @@
 
 namespace App\Actions;
 
+use Hyde\Hyde;
+use Hyde\Foundation\HydeKernel;
 use Hyde\Framework\Features\BuildTasks\PostBuildTask;
+use function json_encode;
+use function file_put_contents;
 
 class ExportKernelJsonBuildTask extends PostBuildTask
 {
     public function handle(): void
     {
-        // TODO: Implement handle() method.
+        file_put_contents(Hyde::sitePath('hyde.json'), json_encode(HydeKernel::getInstance(), JSON_PRETTY_PRINT));
     }
 }
