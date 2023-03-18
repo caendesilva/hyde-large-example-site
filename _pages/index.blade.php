1a4d20ee36cba5f9875bea89fa389c3e3e239d06
Caen De Silva <caen@desilva.se>
2023-03-18 17:08:36 +0100

    Replace Markdown index page with a Blade one

---
+++ _pages/index.blade.php
@@ -0,0 +1,9 @@
+@extends('hyde::layouts.app')
+@section('content')
+@php($title = 'Home')
+
+    <main id="content" class="mx-auto max-w-7xl py-16 px-8">
+        <h1 class="text-center text-3xl font-bold">A <a href="https://github.com/hydephp/hyde">HydePHP</a> site with lots of content</h1>
+    </main>
+
+@endsection
--- _pages/index.md
+++
@@ -1,5 +0,0 @@
----
-title: "Home"
----
-
-# A [HydePHP](https://github.com/hydephp/hyde) site with lots of content
