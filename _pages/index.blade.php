b8a34303e9638dfa917fde4d4701c9dd25fd8e62
Caen De Silva <caen@desilva.se>
2023-03-18 17:42:00 +0100

    Add intro section

--- _pages/index.blade.php
+++ _pages/index.blade.php
@@ -4,8 +4,13 @@
 
     <main id="content" class="mx-auto max-w-7xl py-16 px-8">
         <h1 class="text-center text-3xl font-bold">A <a href="https://github.com/hydephp/hyde">HydePHP</a> site with lots of content</h1>
+        <article class="mx-auto max-w-3xl mt-8 prose dark:prose-invert">
+            <p class="lead text-center">
+                This is a testing site for <a href="https://github.com/hydephp/hyde">HydePHP</a>. It has a lot of content to test various functionality.
+            </p>
+        </article>
 
-        <section class="mx-auto max-w-3xl py-8 mt-8 prose dark:prose-invert">
+        <section class="mx-auto max-w-3xl py-8 prose dark:prose-invert">
             <h2>Sitemap</h2>
             <ul>
                 @foreach (\Hyde\Hyde::pages() as $page)
