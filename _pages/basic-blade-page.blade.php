577ebe7f9918d1e0518e3c7d177ec374b8060621
Caen De Silva <caen@desilva.se>
2023-03-18 14:11:55 +0100

    Add sample Html

--- _pages/basic-blade-page.blade.php
+++ _pages/basic-blade-page.blade.php
@@ -4,6 +4,67 @@
 
 <main class="mx-auto max-w-7xl py-16 px-8">
     <h1 class="text-center text-3xl font-bold">Basic Blade Page</h1>
+
+    <article class="prose dark:prose-invert mt-8 mx-auto">
+        <h2 id="tectaque-ab-stipite-victarum-iuvenis-at-rediere">Tectaque ab stipite victarum iuvenis at rediere</h2>
+        <p>Lorem markdownum discedite faveant deum, talis ibi praecordiaque <strong>poena</strong>;
+            indicere. Condita horrore ille spretarumque caput discordia <em>tectam levibus</em>
+            aetate odorato me. Tellure carinas et vidit et ademit quae, tendensque,
+            fratribus. Agricolis feror, <em>properata volitare</em>: sonat vincula. Nunc non
+            pependit quascumque totidem totiens aderat qui concipiunt probatur: habuere esse
+            accede ultro et adnuit operum.</p>
+        <blockquote>
+            <p>Hiemalibus ut suasit Cycnus est guttis quascumque conubia circumvelatur fluit
+                pugnantem scelus Nyseus. Mihi tulit sed versuta victor, iuvenes tu praesepibus
+                talia temporis scopulum coniunx nimiumque. Profatur <em>fugacibus</em> area
+                Taygetenque pati vestes miserae: non, data rapit lapides quondam, sit igne!
+                Saepe est non inque ducite tuum Phaethon pugna torique Melaneus, altis desere
+                dentibus fama indicii in recentes. Epulis gradu videbit; ego voverat primo,
+                armentaque Myrrha Hymenaeus, amatam.</p>
+        </blockquote>
+        <h2 id="linguae-ut-aristas-fluxit-bracchia-si-fregit">Linguae ut aristas fluxit bracchia si fregit</h2>
+        <p>Huius retentas, me recentes accipis blandarum imperium temptat domum? Neve arma
+            Myrrhae potes, crocique arcum refert rursus multi qua illa amplecti. Generi
+            diligis. Modo barba, ubi de sanguine excita, nunc sine plagis visam.</p>
+        <pre><code>icf += sampleTrackback * 4 + bootBoot + nameDesignSprite;
+joystick -= 3;
+if (scanner + dithering &lt; -5) {
+    unfriend_json(ethernet, overwriteMenu);
+    biometrics(5, qwerty, 1);
+} else {
+    screenshot_compiler_font(toslink_linkedin_default / io);
+}
+matrix_wysiwyg_default(heuristic.balance_adc_nosql(
+        kilobyteSoftwareInternet));
+multiprocessingOdbcExecutable.processorCutSsid += copy;
+</code></pre>
+        <h2 id="iuvenaliter-arduus">Iuvenaliter arduus</h2>
+        <p>Loquarque et peregit! Ratae priorum, in ipse Pelasgae superosque neve ardentibus
+            patres aliquid, clavae confundimur manusque ventura. Movere fuit dote harenis
+            meminisse nate sonumque, posito requiem, quoque munera tamen et pectore vices:
+            palmis. Gestae Bootes fatus nihil fortissimus mutati tenuisse carchesia mihi.</p>
+        <ol>
+            <li>Est deorum amore aper necem iam quicquam</li>
+            <li>Tangamque observata et unda vietum herbis dextra</li>
+            <li>Tulere temporis</li>
+            <li>Titania ferre parvos</li>
+            <li>Candidus venae</li>
+        </ol>
+        <h2 id="spectat-nostri">Spectat nostri</h2>
+        <p>Tam pictis salutem indignante ungues. Modo videre unda est ab non hanc rabieque
+            dolor quae suos quorum silvas Cephison et frustra, citharam. Nocte leti ictibus
+            flammaque quippe; Peleus, mira! Et lupus relictum tendit, est nudos vinctoque.</p>
+        <ul>
+            <li>Qui qua</li>
+            <li>Capitolia fuit delere</li>
+            <li>Illa inlisit</li>
+            <li>Oris vivo</li>
+        </ul>
+        <p>Et auro, guttae inspirare prole in nec urna requies Stygius duraque latuerunt,
+            vellere? Ne <a href="http://www.deos-abeunt.net/">iuga</a>, quod suo o orbatura movebere,
+            inquit quod! Cornibus et mihi non muros, Pallantias subiecta suis?</p>
+
+    </article>
 </main>
 
 @endsection
